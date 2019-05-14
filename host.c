6b3b9a9cf6594cae6a12ada17f16b05a9a9ee277
James Smith <james-smith-za@users.noreply.github.com>
2019-05-14 14:51:48 +0200

    end-of-day commit, I've had enough, I think.

--- host.c
+++ host.c
@@ -13,12 +13,13 @@ struct host *create_host(char type)
     struct host *new_host = malloc(sizeof(*new_host));
     if (new_host != NULL)
     {
-        my_host->update_sensor_value = host_update_sensor_value_function;
+        new_host->update_sensor_value = host_update_sensor_value_function;
         new_host->host_type = type;
         switch (type)
         {
             case 'f':
-                /* TODO allocate a couple of fengine sensors */
+                new_host->number_of_sensors = 0;
+                add_sensor(new_host, "network");
                 break;
             case 'x':
                 /* TODO allocate a couple of xengine sensors */

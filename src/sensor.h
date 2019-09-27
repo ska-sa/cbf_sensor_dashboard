e7da6503a1a6c782c38212c8d8c3608df50e4232
James Smith <james-smith-za@users.noreply.github.com>
2019-09-27 13:49:14 +0200

    Added doxygen information to sensor class.

--- src/sensor.h
+++ src/sensor.h
@@ -1,15 +1,13 @@
 #ifndef _SENSOR_H_
 #define _SENSOR_H_
 
+/**
+ * \file   sensor.h
+ * \brief  Header file for the sensor type.
+ */
+
 struct sensor;
 
-/**
- * \brief  Create a sensor object.
- *
- * \detail Allocate memory for a sensor object and populate the members
- *         with sensible default values.
- *
- */
 struct sensor *sensor_create(char *new_name);
 void sensor_destroy(struct sensor *this_sensor);
 char *sensor_get_name(struct sensor *this_sensor);

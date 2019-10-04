bda91042950968f09beadb068b22cbe5e47ec656
James Smith <james-smith-za@users.noreply.github.com>
2019-10-04 14:34:39 +0200

    Made tokenise more robust, need to use it in the web resource request bit.

--- src/tokenise.c
+++ src/tokenise.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 #include "tokenise.h"
-#include "verbose.h"
 
 size_t tokenise_string(char* input_string, char delim, char ***tokens)
 {
@@ -17,34 +16,46 @@ size_t tokenise_string(char* input_string, char delim, char ***tokens)
 
     size_t previous_delim_location = 0;
 
-    //verbose_message(BORING, "Tokenise: string starting out as: %s\n", string);
+    int prev_state = 0;
+    int curr_state;
 
     size_t num_tokens = 0;
 
     size_t i;
     for (i = 0; i < string_len; i++)
     {
+        //check if current character matches the delimiter
         if (string[i] == delim)
+            curr_state = 0;
+        else
+            curr_state = 1;
+
+        if (prev_state == 0 && curr_state == 1) //word starts
+        {
+            previous_delim_location = i;
+        }
+        else if (prev_state == 1 && curr_state == 0) //word ends, add token
         {
             string[i] = 0;
             (*tokens) = realloc((*tokens), sizeof(*(*tokens))*(num_tokens + 1));
             (*tokens)[num_tokens] = malloc(strlen(string + previous_delim_location) + 1);
             strcpy((*tokens)[num_tokens], string + previous_delim_location);
             num_tokens++;
-            previous_delim_location = i + 1;
-            //verbose_message(BORING, "Tokenise: string is now: %s\n", string + previous_delim_location);
-
         }
+        prev_state = curr_state;
     }
-    //for the last token on the string.
-    (*tokens) = realloc((*tokens), sizeof(*(*tokens))*(num_tokens + 1));
-    (*tokens)[num_tokens] = malloc(strlen(string + previous_delim_location) + 1);
-    strcpy((*tokens)[num_tokens], string + previous_delim_location);
-    num_tokens++;
+    //at the end of the string
+    if (curr_state == 1) //there was a word right at the end of the string.
+    {
+        string[i] = 0;
+        (*tokens) = realloc((*tokens), sizeof(*(*tokens))*(num_tokens + 1));
+        (*tokens)[num_tokens] = malloc(strlen(string + previous_delim_location) + 1);
+        strcpy((*tokens)[num_tokens], string + previous_delim_location);
+        num_tokens++;
+    }
 
     free(string);
     string = NULL;
 
-    //verbose_message(BORING, "Tokenise: returning %d.\n", num_tokens);
     return num_tokens;
 }
